9aba73f09b7d490b89ae93a28a9fe29cc97cc48d
Patrick Walsh <pmwals09@gmail.com>
2025-05-16 14:49:12 -0400

    base functionality

--- go.mod
+++ go.mod
@@ -2,7 +2,12 @@ module github.com/pmwals09/git-pretty-log
 
 go 1.24.2
 
-require github.com/go-git/go-git/v5 v5.16.0
+require (
+	github.com/fatih/color v1.18.0
+	github.com/go-git/go-git/v5 v5.16.0
+	github.com/jedib0t/go-pretty/v6 v6.6.7
+	github.com/maniartech/gotime v1.1.0
+)
 
 require (
 	dario.cat/mergo v1.0.0 // indirect
@@ -16,12 +21,17 @@ require (
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/pjbgf/sha1cd v0.3.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/skeema/knownhosts v1.3.1 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"io"
 	"os"
+	"slices"
+	"strings"
 
+	"github.com/fatih/color"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/maniartech/gotime"
 )
 
 func main() {
@@ -18,15 +26,61 @@ func main() {
 	}
 
 	// get the repo base branch reference
-	_, err = getBaseBranch(repo)
+	baseBranch, err := getBaseBranch(repo)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err.Error())
 		os.Exit(1)
 	}
+	baseCommit, err := repo.CommitObject(baseBranch.Hash())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error getting base branch commit: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	// Map local branch hashes to branch name
+	refs, err := repo.References()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error getting repo references: %s\n", err.Error())
+		os.Exit(1)
+	}
+	refHashToName := make(map[string][]string)
+	refs.ForEach(func(r *plumbing.Reference) error {
+		refHash := r.Hash().String()
+		if _, ok := refHashToName[refHash]; ok {
+			refHashToName[refHash] = append(refHashToName[refHash], r.Name().Short())
+		} else {
+			refHashToName[refHash] = []string{r.Name().Short()}
+		}
+		return nil
+	})
+
+	table := getTableWriter()
 
 	// start walking back 30 commits
-	// if commit contains master, produce a diff
-	// else print everything else
+	log, err := repo.Log(&git.LogOptions{})
+	for i := 0; i < 30; i++ {
+		commit, err := log.Next()
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				fmt.Fprintf(os.Stderr, "error walking log: %s", err.Error())
+				os.Exit(1)
+			} else {
+				break
+			}
+		}
+
+		// if commit contains master, produce a diff
+		if contains, err := commitContainsCommit(commit, baseCommit); err != nil {
+			fmt.Fprintf(os.Stderr, "error getting ancestry: %s\n", err.Error())
+			os.Exit(1)
+		} else if contains {
+			printCommitWithDiff(commit, baseCommit, &table, refHashToName)
+		} else {
+			printCommit(commit, &table, refHashToName)
+		}
+	}
+
+	table.Render()
 }
 
 func getRepo() (*git.Repository, error) {
@@ -50,4 +104,106 @@ func getBaseBranch(repo *git.Repository) (*plumbing.Reference, error) {
 	}
 
 	return nil, errors.New("unable to find base branch among \"main\" or \"master\"")
+}
+
+func commitContainsCommit(descendant, ancestor *object.Commit) (bool, error) {
+	mb, err := descendant.MergeBase(ancestor)
+	if err != nil {
+		return false, err
+	}
+	if len(mb) == 0 {
+		return false, nil
+	}
+
+	contains := slices.ContainsFunc(mb, func(m *object.Commit) bool {
+		return m.Hash == ancestor.Hash
+	})
+	return contains, nil
+}
+
+func getTableWriter() table.Writer {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.Style().Options.DrawBorder = false
+	t.Style().Options.SeparateColumns = false
+	t.Style().Options.SeparateFooter = false
+	t.Style().Options.SeparateHeader = false
+	t.Style().Options.SeparateRows = false
+	return t
+}
+
+func printCommit(commit *object.Commit, tw *table.Writer, refHashToName map[string][]string) {
+	hash := prettyHash(commit)
+	relTime := prettyRelativeTime(commit)
+	author := prettyAuthor(commit)
+	diff := ""
+	message := prettyMessage(commit, refHashToName)
+	(*tw).AppendRow(table.Row{hash, relTime, author, diff, message})
+}
+
+func printCommitWithDiff(commit, ancestor *object.Commit, tw *table.Writer, refHashToName map[string][]string) {
+	hash := prettyHash(commit)
+	relTime := prettyRelativeTime(commit)
+	author := prettyAuthor(commit)
+	diff := prettyDiff(commit, ancestor)
+	message := prettyMessage(commit, refHashToName)
+	(*tw).AppendRow(table.Row{hash, relTime, author, diff, message})
+}
+
+func prettyHash(commit *object.Commit) string {
+	return color.YellowString(commit.Hash.String()[:7])
+}
+func prettyRelativeTime(commit *object.Commit) string {
+	return color.GreenString(gotime.TimeAgo(commit.Author.When))
+}
+func prettyAuthor(commit *object.Commit) string {
+	return color.New(color.FgBlue).Add(color.Bold).Sprint(commit.Author.Name)
+}
+func prettyMessage(commit *object.Commit, refHashToName map[string][]string) string {
+	var refName string
+	if refNames, ok := refHashToName[commit.Hash.String()]; ok {
+		formattedRefNames := make([]string, 0, len(refNames))
+		for _, rn := range refNames {
+			formattedRefNames = append(formattedRefNames, color.RedString("(%s)", rn))
+		}
+		refName = strings.Join(formattedRefNames, " ")
+		return fmt.Sprintf("%s %s", refName, strings.TrimSpace(commit.Message))
+	} else {
+		return strings.TrimSpace(commit.Message)
+	}
+}
+
+type DiffItem struct {
+	value int
+	token rune
+	color *color.Color
+}
+
+func prettyDiff(commit, ancestor *object.Commit) string {
+	commitTree, _ := commit.Tree()
+	ancestorTree, _ := ancestor.Tree()
+	changes, _ := object.DiffTreeWithOptions(context.Background(), ancestorTree, commitTree, &object.DiffTreeOptions{DetectRenames: true})
+	p, _ := changes.Patch()
+	stats := p.Stats()
+	var totalFiles int
+	var totalAdded int
+	var totalDeleted int
+	for _, s := range stats {
+		totalFiles++
+		totalAdded += s.Addition
+		totalDeleted += s.Deletion
+	}
+	items := []DiffItem{
+		{value: totalFiles, token: '~', color: color.New(color.FgYellow)},
+		{value: totalAdded, token: '+', color: color.New(color.FgGreen)},
+		{value: totalDeleted, token: '-', color: color.New(color.FgRed)},
+	}
+	selectedItems := make([]string, 0, 3)
+	for _, item := range items {
+		if item.value > 0 {
+			formattedItem := item.color.Sprintf("%d(%c)", item.value, item.token)
+			selectedItems = append(selectedItems, formattedItem)
+		}
+	}
+	return strings.Join(selectedItems, ", ")
 }
